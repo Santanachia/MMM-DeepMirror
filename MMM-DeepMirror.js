c22c38c3accda73061e91ee4241d428062151f5c
Marcin Bielecki <santanachia@gmail.com>
2017-04-21 14:00:17 +0200

    use svg,js instead of pure JS

--- MMM-DeepMirror.js
+++ MMM-DeepMirror.js
@@ -8,6 +8,12 @@ Module.register("MMM-DeepMirror", {
     color: '#aaa',
   },
 
+  getScripts: function () {
+    return [
+      '//rawgit.com/svgdotjs/svg.js/master/dist/svg.min.js'
+    ];
+  },
+
   start: function () {
     var body = document.body, html = document.documentElement;
 
@@ -15,8 +21,7 @@ Module.register("MMM-DeepMirror", {
     var w = Math.max(body.scrollWidth, body.offsetWidth, html.clientWidth, html.scrollWidth, html.offsetWidth)
 
     this.svgElem = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-    this.svgElem.setAttributeNS(null, 'width', w);
-    this.svgElem.setAttributeNS(null, 'height', h);
+    var draw = SVG.adopt(this.svgElem).size(w, h)
 
     var dx = Math.round(w / this.config.spacing)
     var dy = Math.round(h / this.config.spacing)
@@ -37,23 +42,8 @@ Module.register("MMM-DeepMirror", {
 
         params.x = startx + j * spacingx
 
-        var svgTop = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-        svgTop.setAttributeNS(null, 'cx', params.x);
-        svgTop.setAttributeNS(null, 'cy', params.y);
-        svgTop.setAttributeNS(null, 'r', params.r);
-        svgTop.setAttributeNS(null, 'fill-opacity', params.o);
-        svgTop.setAttributeNS(null, 'fill', this.config.color);
-
-        this.svgElem.appendChild(svgTop);
-
-        var svgBottom = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-        svgBottom.setAttributeNS(null, 'cx', params.x);
-        svgBottom.setAttributeNS(null, 'cy', h - params.y);
-        svgBottom.setAttributeNS(null, 'r', params.r);
-        svgBottom.setAttributeNS(null, 'fill-opacity', params.o);
-        svgBottom.setAttributeNS(null, 'fill', this.config.color);
-
-        this.svgElem.appendChild(svgBottom);
+        draw.circle(params.r).fill({ color: this.config.color, opacity: params.o }).move(params.x, params.y)
+        draw.circle(params.r).fill({ color: this.config.color, opacity: params.o }).move(params.x, h - params.y)
 
       }
 
@@ -62,27 +52,14 @@ Module.register("MMM-DeepMirror", {
 
         params.y = starty + j * spacingy
 
-        var svgLeft = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-        svgLeft.setAttributeNS(null, 'cx', params.x);
-        svgLeft.setAttributeNS(null, 'cy', params.y);
-        svgLeft.setAttributeNS(null, 'r', params.r);
-        svgLeft.setAttributeNS(null, 'fill-opacity', params.o);
-        svgLeft.setAttributeNS(null, 'fill', this.config.color);
-
-        this.svgElem.appendChild(svgLeft);
-
-        var svgRight = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-        svgRight.setAttributeNS(null, 'cx', w - params.x);
-        svgRight.setAttributeNS(null, 'cy', params.y);
-        svgRight.setAttributeNS(null, 'r', params.r);
-        svgRight.setAttributeNS(null, 'fill-opacity', params.o);
-        svgRight.setAttributeNS(null, 'fill', this.config.color);
-
-        this.svgElem.appendChild(svgRight);
+        draw.circle(params.r).fill({ color: this.config.color, opacity: params.o }).move(params.x, params.y)
+        draw.circle(params.r).fill({ color: this.config.color, opacity: params.o }).move(w - params.x, params.y)
 
       }
     }
 
+    //this.svgElem = draw.doc()
+    //this.svgElem = draw.svg()
   },
 
   // Override dom generator.
